61dde9f3058e5a012afa6afef73a43d5b034dc7a
哘雅楽乃 <16664369+Umineko1993@users.noreply.github.com>
2023-06-30 04:41:51 +0900

    Add files via upload
    Signed-off-by: 哘雅楽乃 <16664369+Umineko1993@users.noreply.github.com>

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JarInfectionScanner")]
+[assembly: AssemblyTitle("Jarマルウェアスキャナー_JP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JarInfectionScanner")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyProduct("Jarマルウェアスキャナー_JP")]
+[assembly: AssemblyCopyright("Copyright ©  2023 Saso-Utano")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: NeutralResourcesLanguage("ja-JP")]
